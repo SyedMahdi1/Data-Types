8dbe7a980ae90c7e6cbc466854312482bf08daf4
SyedMahdi1 <131530472+SyedMahdi1@users.noreply.github.com>
2024-04-03 05:10:32 -0700

    Enjoy The Codes
    New Lesson YEAH:) 😎😎😎😎

---
+++ DataTypes.py
@@ -0,0 +1,18 @@
+a = "Mahdi"
+print(type(a))
+b = 1
+print(type(b))
+c = 1.2
+print(type(c))
+d = (1, 2, 3)
+print(type(d))
+e = True
+print(type(e))
+f = False
+print(type(f))
+g = None
+print(type(g))
+h = {1, 2}
+print(type(h))
+i = [1, 2]
+print(type(i))
